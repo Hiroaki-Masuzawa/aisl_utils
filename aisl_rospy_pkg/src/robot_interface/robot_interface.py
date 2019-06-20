953f220b06ec31099a62ae497e4d8e94b7546822
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2019-06-21 00:33:58 +0900

    Initial commit

---
+++ aisl_rospy_pkg/src/robot_interface/robot_interface.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import rospy
+
+class RobotInterface(object):
+    def __init__(self):
+        # rospy.loginfo('[RobotInterface] Constructor called')
+        print('[RobotInterface] Constructor called')
+
+        self.name = 'Robot'
+
+    def pass_object(self):
+        # rospy.logwarn('[RobotInterface] pass_object() not implemented yet')
+        print('[RobotInterface] pass_object() not implemented yet')
+
+    def speak(self):
+        # rospy.logwarn('[RobotInterface] speak() not implemented yet')
+        print('[RobotInterface] speak() not implemented yet')
+
+    def fetch_object(self):
+        # rospy.logwarn('[RobotInterface] fetch_object() not implemented yet')
+        print('[RobotInterface] fetch_object() not implemented yet')
+
+    def put_object(self):
+        # rospy.logwarn('[RobotInterface] put_object() not implemented yet')
+        print('[RobotInterface] put_object() not implemented yet')
