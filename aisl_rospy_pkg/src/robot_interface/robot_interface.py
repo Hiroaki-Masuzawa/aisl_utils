fea1d8ea64a801d259470d82113656b9b374e4fe
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2019-06-21 11:57:57 +0900

    [add] Define srv files for robot_interface

--- aisl_rospy_pkg/src/robot_interface/robot_interface.py
+++ aisl_rospy_pkg/src/robot_interface/robot_interface.py
@@ -24,3 +24,7 @@ class RobotInterface(object):
     def put_object(self):
         # rospy.logwarn('[RobotInterface] put_object() not implemented yet')
         print('[RobotInterface] put_object() not implemented yet')
+
+    def move_to_neutral(self):
+        # rospy.logwarn('[RobotInterface] put_object() not implemented yet')
+        print('[RobotInterface] move_to_neutral() not implemented yet')
